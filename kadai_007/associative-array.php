27bb8ca0f9ea7291e700b09d416e488262314add
mikako-m <chinatsu.m.19.06.19@gmail.com>
2024-10-01 19:05:08 +0900

    kadai007アップロード

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+ <html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <title>連想配列を作って値とキーを出力しよう</title>
+ </head>
+ <body>
+  <p>
+    <?php
+    $Array=['name'=>'onion','price'=> 200,'weight'=> 160];
+    print_r($Array);
+    ?>
+  </p>
+  </body>
+</html>
